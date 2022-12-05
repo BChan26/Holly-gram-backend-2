c51f1af6b89fab1b6d997a286dc6768f0cf61cd2
Lindsey Lawler <lawlerlindsey@gmail.com>
2022-12-05 10:00:27 -0500

    usercontroller added

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,4 +1,19 @@
+const { User } = require('../models')
+
 
 const getUsers = (req, res)=>{
     res.send({msg: `Found users with an id of ${req.params.userId}`})
+}
+
+const SeeOneUser = async (req,res) => {
+    try {
+        const post = await User.findOne({where: {id: req.body.postId}})
+        res.send(post)
+       } catch (error) {
+        throw error
+       }
+}
+
+module.exports = {
+    SeeOneUser
 }
