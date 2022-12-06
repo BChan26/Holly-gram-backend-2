81c4942fd7ff3e4a3c1c8423f8e3a4aa15307941
Lindsey Lawler <lawlerlindsey@gmail.com>
2022-12-06 13:09:25 -0500

    comment routes added

---
+++ routes/CommentRoute.js
@@ -0,0 +1,29 @@
+const router = require('express').Router()
+const controller = require('../controllers/Comments')
+const middleware = require('../middleware')
+
+
+
+
+router.post(
+    '/',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.CreateComment
+)
+
+router.put(
+    ':comment_id', //<might need to be comments_id
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.UpdateComment
+)
+
+router.delete(
+    '/:comment_id',
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.DeleteComment
+)
+
+module.exports = router
